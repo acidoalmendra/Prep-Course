b463425db7c705cb47d7256aacea96afc5b667ec
Agustín S. Almonacid <agustinsalmonacid@gmail.com>
2022-11-19 23:45:18 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'Salud';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -18,7 +18,7 @@ const nuevaMultiplicacion = 10 * null === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
 
-
+gi
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
